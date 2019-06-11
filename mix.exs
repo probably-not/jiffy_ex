f040a4fe42086aab65100d5513d2e8c60d9994e0
Coby Benveniste <coby.b@spot.im>
2019-06-11 09:35:57 +0300

    Version bump v1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JiffyEx.MixProject do
   def project do
     [
       app: :jiffy_ex,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
